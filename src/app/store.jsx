2bfe456680cfa86f060c055b914bb9f3a5717306
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-07-16 16:32:32 +0200

    Updates

--- src/app/store.jsx
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import photosReducer from '../features/photos/photosSlice'
-
-export const store = configureStore({
-  reducer: {
-    photos: photosReducer
-  },
-})
