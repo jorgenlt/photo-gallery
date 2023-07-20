f5080bd663d1e84cf2c93997260baff04a8f13eb
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-07-20 18:33:57 +0200

    firebase config

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCPjRrG0qhIKyj1D_NkpLVSDMZDlj8Xps8",
+  authDomain: "photo-gallery-02.firebaseapp.com",
+  projectId: "photo-gallery-02",
+  storageBucket: "photo-gallery-02.appspot.com",
+  messagingSenderId: "214278098284",
+  appId: "1:214278098284:web:4d58ab177b7fad61a4f749"
+}; 
+
+export default firebaseConfig;
