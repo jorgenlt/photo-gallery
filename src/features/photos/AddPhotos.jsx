7acd2a15bc483836abac2428dbd03339728d0330
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-07-22 12:49:26 +0200

    save

--- src/features/photos/AddPhotos.jsx
+++ src/features/photos/AddPhotos.jsx
@@ -1,54 +1,76 @@
-import { useState } from 'react'
-import axios from 'axios';
+import { useState } from 'react';
+import { initializeApp } from "firebase/app";
+import { getStorage, ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+
+import { firebaseConfig } from '../../../firebaseConfig';
 
 const AddPhotos = () => {
   const [location, setLocation] = useState('');
   const [category, setCategory] = useState('');
-  const [image, setImage] = useState('');
+  const [image, setImage] = useState(null);
+  
+  const app = initializeApp(firebaseConfig);
 
-  const cloudName = 'dpqspser3';
-  // const apiKey = import.meta.env.VITE_API_KEY; 
-  // const apiSecret = import.meta.env.VITE_API_SECRET;
+  // Initialize Cloud Storage and get a reference to the service
+  const storage = getStorage(app);
 
   const uploadImage = () => {
-    const uploadOptions = { tags: ['photo-gallery', location, category] };
-    const formData = new FormData();
-    formData.append('file', image);
-    formData.append('upload_preset', 'uhel5qqu');
+    if(image) {
+      // Upload file and metadata to the object 'images/${image.name}'
+      const storageRef = ref(storage, image.name);
 
-    axios.post(
-      `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
-      formData,
-      { params: uploadOptions }
-      )
-      .then(response => {
-        console.log(`${response.status}, ${response.statusText}`);
-        console.log(response);
-        alert('Image was successfully uploaded.')
-      })
-      .catch(error => {
-        console.error(error);
-      })
-    
-    setImage('');
-    setLocation('');
-    setCategory('');
-  }  
+      const uploadTask = uploadBytesResumable(storageRef, image);
 
+      uploadTask.on('state_changed', 
+        (snapshot) => {
+          // Get task progress
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log('Upload is ' + progress + '% done');
+        }, 
+        (error) => {
+          // Handle errors
+          console.log(error);
+        }, 
+        () => {
+          // Upload completed successfully, now we can get the download URL
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            console.log('File available at', downloadURL);
+          });
+        }
+      );
+    }
+  }
+  
   return (
     <div className="add-photos">
       <h2>Add photos</h2>
+
       <div>
         <form>
-          <input type="text" placeholder="Location" value={location} onChange={e => setLocation(e.target.value)} />
-          <input type="text" placeholder="Category" value={category} onChange={e => setCategory(e.target.value)} />
-          <input type="file" onChange={e => setImage(e.target.files[0])}/>
+          <input 
+            type="text" 
+            placeholder="Location"
+            value={location} 
+            onChange={e => setLocation(e.target.value)} 
+          />
+          
+          <input
+            type="text"
+            placeholder="Category"
+            value={category}
+            onChange={e => setCategory(e.target.value)}  
+          />
+          
+          <input 
+            type="file"
+            onChange={e => setImage(e.target.files[0])}
+          />
         </form>
-        <button type='submit' onClick={uploadImage}>Upload</button>
+
+        <button onClick={uploadImage}>Upload</button>
       </div>
     </div>
   )
 }
 
-
-export default AddPhotos
+export default AddPhotos;
